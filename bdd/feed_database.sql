0706897090f58ef3c270619805aa063c8fd6e085
bleutn <bleutn@gmail.com>
2015-07-22 16:56:22 +0200

    [fix] modification du modèle entité Article
    [fix] modification du modèle entité Article

--- bdd/feed_database.sql
+++ bdd/feed_database.sql
@@ -9,5 +9,5 @@ INSERT INTO orderstatus (label, created) VALUES ('Commandé', NOW());
 INSERT INTO orderstatus (label, created) VALUES ('Non Commandé', NOW());
 
 
-INSERT INTO articles (category_id, price, label, description, is_ordered, is_on_sale, status_id, created)
+INSERT INTO articles (category_id, price, label, description, is_ordered, is_sale, status_id, created)
 VALUES (1, 12.53, 'Produit 1', 'Description produit 1', 0, 1, 1, NOW());
